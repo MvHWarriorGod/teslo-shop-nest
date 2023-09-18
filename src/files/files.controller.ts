6b356a2bd72a86143edc3cbc0c35bdc6b7334924
Mariano Vega <marianovh2000@gmail.com>
2023-09-18 17:06:47 -0500

    Agregar imagenes

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,19 +1,47 @@
 import {
   Controller,
   Post,
+  Get,
   UploadedFile,
   UseInterceptors,
   ParseFilePipe,
   FileTypeValidator,
+  Res,
+  Param,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { fileNamer } from './helpers/fileNamer.helper';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+
+    res.sendFile(path);
+  }
+
   @Post('product')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
   UploadProductImage(
     @UploadedFile(
       new ParseFilePipe({
@@ -25,6 +53,10 @@ export class FilesController {
     )
     file: Express.Multer.File,
   ) {
-    return file;
+    // const secureUrl = `${file.filename}`;
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${
+      file.filename
+    }`;
+    return { secureUrl };
   }
 }
